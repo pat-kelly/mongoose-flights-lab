53a33b500bf9e403b1d5bd7a745fd70da50947bf
Patrick <kelly.patj@gmail.com>
2023-01-10 14:49:19 -0500

    fix / -- adjusting flight model

--- models/flight.js
+++ models/flight.js
@@ -2,10 +2,26 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const movieSchema = new Schema({
-  airline: String,
-  airport: String,
-  flightNo: Number,
-  departs: Date
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    default: 'DEN'
+  },
+  flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999
+  },
+  departs: {
+    type: Date,
+    default: function(){
+      return new Date();
+    }
+  }
 
 }, {timestamps: true})
 
